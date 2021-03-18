323a80218718b98d27c5094ac998b46c179b7f9f
Jacob Fløe Jeppesen <jajep18@student.sdu.dk>
2021-03-18 11:05:56 +0100

    Started work on LIDAR class

--- includes/LIDAR.h
+++ includes/LIDAR.h
@@ -1,5 +1,24 @@
 #pragma once
+/*
+ * Description:     Vision (camera and detection) class
+ *
+ * Author:			Jacob Fløe Jeppesen
+ *					University of Southern Denmark
+ * Creation date:   18-03-2021
+ */
+#include <rplidar.h>
+#include <iostream>
+#include "defines.h"
+
 class LIDAR
 {
+
+private:
+	RPlidarDriver* lidar;
+
+public:
+	LIDAR();
+	~LIDAR();
+
 };
 
--- sources/LIDAR.cpp
+++ sources/LIDAR.cpp
@@ -1,1 +1,35 @@
 #include "../includes/LIDAR.h"
+
+LIDAR::LIDAR()
+{
+	lidar = RPlidarDriver::CreateDriver();
+
+	u_result res = lidar->connect("/dev/ttyUSB0", 115200);
+
+	if (IS_OK(res))
+	{
+		std::cout << "Lidar connected!\n";
+		lidar->startMotor();
+		std::cout << "Lidar motor started!\n";
+
+		for (size_t i = 0; i < 10; i++)
+		{
+			std::cout << "Waiting... \n";
+		}
+		
+	}
+	else
+	{
+		fprintf(stderr, "Failed to connect to LIDAR %08x\r\n", res);
+	}
+
+	lidar->stopMotor();
+	std::cout << "Lidar motor stopped!\n";
+	lidar->disconnect();
+	std::cout << "Lidar disconnected!\n";
+}
+
+LIDAR::~LIDAR()
+{
+	RPlidarDriver::DisposeDriver(lidar);
+}
