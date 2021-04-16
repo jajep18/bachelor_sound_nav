cdb4bb43338f196962b21a2b4bfa7b27a1d9a592
Jacob Fløe Jeppesen <jajep18@student.sdu.dk>
2021-04-16 11:11:53 +0200

    Found a better example to work from.

--- testOfYOLO.cpp
+++ testOfYOLO.cpp
@@ -1,61 +1,112 @@
-#include <opencv2/core.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
-
-#include <iostream>
-#include <unistd.h>
-#include <raspicam/raspicam.h>
-#include <atomic>
+// This code is written at BigVision LLC. It is based on the OpenCV project. It is subject to the license terms in the LICENSE file found in this distribution and at http://opencv.org/license.html
+//Source: https://github.com/spmallick/learnopencv/blob/master/ObjectDetection-YOLO/object_detection_yolo.cpp
+// Usage example:  ./object_detection_yolo.out --video=run.mp4
+//                 ./object_detection_yolo.out --image=bird.jpg
 #include <fstream>
-using namespace std;
-// Initialize the parameters
+#include <sstream>
+#include <iostream>
 
-int main(void){
-    float confThreshold = 0.5; // Confidence threshold
-    float nmsThreshold = 0.4;  // Non-maximum suppression threshold
-    int inpWidth = 416;        // Width of network's input image
-    int inpHeight = 416;       // Height of network's input image
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+const char* keys =
+"{help h usage ? | | Usage examples: \n\t\t./object_detection_yolo.out --image=dog.jpg \n\t\t./object_detection_yolo.out --video=run_sm.mp4}"
+"{image i        |<none>| input image   }"
+"{video v       |<none>| input video   }"
+"{device d       |<cpu>| input device   }"
+;
+using namespace cv;
+using namespace dnn;
+using namespace std;
+
+// Initialize the parameters
+float confThreshold = 0.5; // Confidence threshold
+float nmsThreshold = 0.4;  // Non-maximum suppression threshold
+int inpWidth = 416;  // Width of network's input image
+int inpHeight = 416; // Height of network's input image
+vector<string> classes;
+
+// Remove the bounding boxes with low confidence using non-maxima suppression
+void postprocess(Mat& frame, const vector<Mat>& out);
+
+// Draw the predicted bounding box
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
+
+// Get the names of the output layers
+vector<String> getOutputsNames(const Net& net);
+
+int main(int argc, char** argv)
+{
+    CommandLineParser parser(argc, argv, keys);
+    parser.about("Use this script to run object detection using YOLO3 in OpenCV.");
+    if (parser.has("help"))
+    {
+        parser.printMessage();
+        return 0;
+    }
     // Load names of classes
     string classesFile = "coco.names";
     ifstream ifs(classesFile.c_str());
     string line;
     while (getline(ifs, line)) classes.push_back(line);
 
+    string device = "cpu";
+    device = parser.get<String>("device");
+
     // Give the configuration and weight files for the model
-    string modelConfiguration = "yolov3.cfg";
-    string modelWeights = "yolov3.weights";
+    String modelConfiguration = "yolov3.cfg";
+    String modelWeights = "yolov3.weights";
 
     // Load the network
     Net net = readNetFromDarknet(modelConfiguration, modelWeights);
-    net.setPreferableBackend(DNN_BACKEND_OPENCV);
-    net.setPreferableTarget(DNN_TARGET_CPU);
 
-outputFile = "yolo_out_cpp.avi";
-if (parser.has("image"))
-{
-    // Open the image file
-    str = parser.get<String>("image");
-    ifstream ifile(str);
-    if (!ifile) throw("error");
-    cap.open(str);
-    str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.jpg");
-    outputFile = str;
-}
-else if (parser.has("video"))
-{
-    // Open the video file
-    str = parser.get<String>("video");
-    ifstream ifile(str);
-    if (!ifile) throw("error");
-    cap.open(str);
-    str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.avi");
-    outputFile = str;
-}
-// Open the webcaom
-else cap.open(parser.get<int>("device"));
+    if (device == "cpu")
+    {
+        cout << "Using CPU device" << endl;
+        net.setPreferableBackend(DNN_TARGET_CPU);
+    }
+    else if (device == "gpu")
+    {
+        cout << "Using GPU device" << endl;
+        net.setPreferableBackend(DNN_BACKEND_CUDA);
+        net.setPreferableTarget(DNN_TARGET_CUDA);
+    }
 
+
+    // Open a video file or an image file or a camera stream.
+    string str, outputFile;
+    VideoCapture cap;
+    VideoWriter video;
+    Mat frame, blob;
+
+    try {
+
+        outputFile = "yolo_out_cpp.avi";
+        if (parser.has("image"))
+        {
+            // Open the image file
+            str = parser.get<String>("image");
+            ifstream ifile(str);
+            if (!ifile) throw("error");
+            cap.open(str);
+            str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.jpg");
+            outputFile = str;
+        }
+        else if (parser.has("video"))
+        {
+            // Open the video file
+            str = parser.get<String>("video");
+            ifstream ifile(str);
+            if (!ifile) throw("error");
+            cap.open(str);
+            str.replace(str.end() - 4, str.end(), "_yolo_out_cpp.avi");
+            outputFile = str;
+        }
+        // Open the webcaom
+        else cap.open(parser.get<int>("device"));
+
+    }
     catch (...) {
         cout << "Could not open the input image/video stream" << endl;
         return 0;
@@ -66,6 +117,9 @@ else cap.open(parser.get<int>("device"));
         video.open(outputFile, VideoWriter::fourcc('M', 'J', 'P', 'G'), 28, Size(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT)));
     }
 
+    // Create a window
+    static const string kWinName = "Deep learning object detection in OpenCV";
+    namedWindow(kWinName, WINDOW_NORMAL);
 
     // Process frames.
     while (waitKey(1) < 0)
@@ -110,73 +164,99 @@ else cap.open(parser.get<int>("device"));
 
     }
 
-    // Get the names of the output layers
-    vector<String> getOutputsNames(const Net & net)
+    cap.release();
+    if (!parser.has("image")) video.release();
+
+    return 0;
+}
+
+// Remove the bounding boxes with low confidence using non-maxima suppression
+void postprocess(Mat& frame, const vector<Mat>& outs)
+{
+    vector<int> classIds;
+    vector<float> confidences;
+    vector<Rect> boxes;
+
+    for (size_t i = 0; i < outs.size(); ++i)
     {
-        static vector<String> names;
-        if (names.empty())
+        // Scan through all the bounding boxes output from the network and keep only the
+        // ones with high confidence scores. Assign the box's class label as the class
+        // with the highest score for the box.
+        float* data = (float*)outs[i].data;
+        for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
         {
-            //Get the indices of the output layers, i.e. the layers with unconnected outputs
-            vector<int> outLayers = net.getUnconnectedOutLayers();
-
-            //get the names of all the layers in the network
-            vector<String> layersNames = net.getLayerNames();
-
-            // Get the names of the output layers in names
-            names.resize(outLayers.size());
-            for (size_t i = 0; i < outLayers.size(); ++i)
-                names[i] = layersNames[outLayers[i] - 1];
-        }
-        return names;
-    }
-
-
-    // Remove the bounding boxes with low confidence using non-maxima suppression
-    void postprocess(Mat& frame, const vector<Mat>& outs)
-    {
-        vector<int> classIds;
-        vector<float> confidences;
-        vector<Rect> boxes;
-
-        for (size_t i = 0; i < outs.size(); ++i)
-        {
-            // Scan through all the bounding boxes output from the network and keep only the
-            // ones with high confidence scores. Assign the box's class label as the class
-            // with the highest score for the box.
-            float* data = (float*)outs[i].data;
-            for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
+            Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
+            Point classIdPoint;
+            double confidence;
+            // Get the value and location of the maximum score
+            minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
+            if (confidence > confThreshold)
             {
-                Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
-                Point classIdPoint;
-                double confidence;
-                // Get the value and location of the maximum score
-                minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
-                if (confidence > confThreshold)
-                {
-                    int centerX = (int)(data[0] * frame.cols);
-                    int centerY = (int)(data[1] * frame.rows);
-                    int width = (int)(data[2] * frame.cols);
-                    int height = (int)(data[3] * frame.rows);
-                    int left = centerX - width / 2;
-                    int top = centerY - height / 2;
+                int centerX = (int)(data[0] * frame.cols);
+                int centerY = (int)(data[1] * frame.rows);
+                int width = (int)(data[2] * frame.cols);
+                int height = (int)(data[3] * frame.rows);
+                int left = centerX - width / 2;
+                int top = centerY - height / 2;
 
-                    classIds.push_back(classIdPoint.x);
-                    confidences.push_back((float)confidence);
-                    boxes.push_back(Rect(left, top, width, height));
-                }
+                classIds.push_back(classIdPoint.x);
+                confidences.push_back((float)confidence);
+                boxes.push_back(Rect(left, top, width, height));
             }
         }
-
-        // Perform non maximum suppression to eliminate redundant overlapping boxes with
-        // lower confidences
-        vector<int> indices;
-        NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
-        for (size_t i = 0; i < indices.size(); ++i)
-        {
-            int idx = indices[i];
-            Rect box = boxes[idx];
-            drawPred(classIds[idx], confidences[idx], box.x, box.y,
-                box.x + box.width, box.y + box.height, frame);
-        }
     }
+
+    // Perform non maximum suppression to eliminate redundant overlapping boxes with
+    // lower confidences
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
+    for (size_t i = 0; i < indices.size(); ++i)
+    {
+        int idx = indices[i];
+        Rect box = boxes[idx];
+        drawPred(classIds[idx], confidences[idx], box.x, box.y,
+            box.x + box.width, box.y + box.height, frame);
+    }
+}
+
+// Draw the predicted bounding box
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+{
+    //Draw a rectangle displaying the bounding box
+    rectangle(frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
+
+    //Get the label for the class name and its confidence
+    string label = format("%.2f", conf);
+    if (!classes.empty())
+    {
+        CV_Assert(classId < (int)classes.size());
+        label = classes[classId] + ":" + label;
+    }
+
+    //Display the label at the top of the bounding box
+    int baseLine;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+    top = max(top, labelSize.height);
+    rectangle(frame, Point(left, top - round(1.5 * labelSize.height)), Point(left + round(1.5 * labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
+    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0, 0, 0), 1);
+}
+
+// Get the names of the output layers
+vector<String> getOutputsNames(const Net& net)
+{
+    static vector<String> names;
+    if (names.empty())
+    {
+        //Get the indices of the output layers, i.e. the layers with unconnected outputs
+        vector<int> outLayers = net.getUnconnectedOutLayers();
+
+        //get the names of all the layers in the network
+        vector<String> layersNames = net.getLayerNames();
+
+        // Get the names of the output layers in names
+        names.resize(outLayers.size());
+        for (size_t i = 0; i < outLayers.size(); ++i)
+            names[i] = layersNames[outLayers[i] - 1];
+    }
+    return names;
 }
